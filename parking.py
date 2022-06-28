4409d2ab8c54b5cfa389c649c4c1a1c43fd8e571
daniel asherov <djasherov@gmail.com>
2022-06-28 06:40:16 -0800

    first commit

---
+++ parking.py
@@ -0,0 +1,144 @@
+#had issues with completing, went to youtube for solutions.... 
+
+
+from random import shuffle
+
+class Vehicle:
+       
+    def __init__(self,wheel_num,car_type):
+        self._car_type = car_type
+        self._wheel_num = wheel_num
+
+          
+    def getType(self):
+        return self._car_type
+    
+    def getWheelNum(self):
+        return self._wheel_num
+
+class Motorcycle(Vehicle): 
+        def __init__(self):
+            Vehicle.__init__(self,2,'MOTORCYCLE')
+
+class Car(Vehicle):  
+        def __init__(self):
+            Vehicle.__init__(self,4,'CAR')
+            
+class Bus(Vehicle):  
+        def __init__(self):
+            Vehicle.__init__(self,6,'BUS')
+
+#%% parking lot
+class ParkingLot:
+    
+    def __init__(self,small_spot_sz, med_spot_sz,big_spot_sz):          
+        self._vehicle_set = []
+        self._total_spots = self.generateOpenSpots(small_spot_sz, med_spot_sz,big_spot_sz)
+
+    # creat a parking lot with X1 small spots, x2 medium spots and x3 big spots    
+    def generateOpenSpots(self,small_spot_sz, med_spot_sz,big_spot_sz):
+        self._total_spots = []   
+
+        for i in range(small_spot_sz):
+            self._total_spots.append(SmallSpot())
+        for i in range(med_spot_sz):
+            self._total_spots.append(MedSpot())
+        for i in range(big_spot_sz):
+            self._total_spots.append(BigSpot())
+            
+        shuffle(self._total_spots)         
+        return self._total_spots
+      
+    
+    def park_new_vec(self,new_vehicle):
+        
+        total_spots_sz = self.getTotalSpotsSz()
+        for i in range(total_spots_sz):
+            if self._total_spots[i].is_open():
+                #set spot to hold the car
+                if self._total_spots[i].park_vehicle(new_vehicle):
+                    print('{} is parking in spot {} (size {}). '.format(new_vehicle.getType(), i, self._total_spots[i].getSpotSize()))
+                    # add car to the parkingLot cars list
+                    self._vehicle_set.append(new_vehicle)
+                    break
+                
+    def is_park_open(self):
+        return (self.getOpenSpotsSz() > 0)
+    
+    # getters
+    def getTotalSpotsSz(self):
+        return len(self._total_spots)
+    
+    def getVehicleSet(self):
+        return self._vehicle_set
+    
+    def getOpenSpotsSz(self):
+        return self.getTotalSpotsSz() - len(self.getVehicleSet())
+    
+    def getAllSpots(self):
+        return self._total_spots
+
+#%%  spot
+            
+class Spot:
+    
+    def __init__(self,spot_sz):
+        self._spot_size = spot_sz
+        self._spot_car = None
+        
+    def is_open(self):
+        return self._spot_car == None
+    
+    def park_vehicle(self,new_vec):
+        can_park = new_vec.getWheelNum()/2 <= self.getSpotSize()
+        if can_park:
+            self._spot_car = new_vec
+        return can_park              
+
+    # getters        
+    def getParkedVec(self):
+        return self._spot_car
+    
+    def getSpotSize(self):
+        return self._spot_size    
+    
+class SmallSpot(Spot):
+    def __init__(self):
+        Spot.__init__(self,1)
+    
+class MedSpot(Spot):
+    def __init__(self):
+        Spot.__init__(self,2)
+        
+class BigSpot(Spot):
+    def __init__(self):
+        Spot.__init__(self,3)
+#%%
+
+
+
+
+
+small_spot_sz= 2
+med_spot_sz = 10
+big_spot_sz = 2
+
+PL = ParkingLot(small_spot_sz, med_spot_sz,big_spot_sz)
+
+
+cycle_sz = 2
+cars_sz = 5
+buses_sz = 1
+
+vec_set = []
+for i in range(cycle_sz):
+    vec_set.append(Motorcycle())
+for i in range(cars_sz):
+    vec_set.append(Car())            
+for i in range(buses_sz):
+    vec_set.append(Bus())
+shuffle(vec_set)
+
+
+for i in range (len(vec_set)):
+    PL.park_new_vec(vec_set[i])
